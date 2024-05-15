74f37340f4a5142c755787578b7023ded2340b30
cissic <cis00sic@gmail.com>
2024-05-15 15:26:07 +0200

    Precautionary commit before running install-mb-packages.el

--- install-packages.el
+++ install-packages.el
@@ -68,7 +68,7 @@ auctex ; in order to have reftex working
   org-ac
   org-ai
   ;org-download
-  org-plus-contrib
+  ; org-plus-contrib
   ;org-mime
   org-ref ; for handling org-mode references https://emacs.stackexchange.com/questions/9767/can-reftex-be-used-with-org-label
   org-special-block-extras
